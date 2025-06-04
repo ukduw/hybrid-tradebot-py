a5b6644ab2a5d717155f9c2d7e6b07d3b8f09e5c
ukduw <edhkmayo@gmail.com>
2025-06-04 10:38:34 +0100

    added trailing stop loss logic, close all open positions at day end, TODO: pipe outcomes to csv

--- alpaca_utils.py
+++ alpaca_utils.py
@@ -27,3 +27,20 @@ def place_order(symbol, qty, side="buy"):
 def close_position(symbol):
     url = f"{BASE_URL}/v2/positions/{symbol}"
     return requests.delete(url, headers=HEADERS).json()
+
+def close_all_positions():
+    url = f"{BASE_URL}/v2/positions"
+    response = requests.delete(url, headers=HEADERS)
+
+    if response.status_code == 207:
+        print("!!! Partial success closing positions")
+    elif response.status_code == 200:
+        print("All positions closed.")
+    else:
+        print(f"Failsed to close positions: {response.status_code} {response.text}")
+
+    if response.status_code in [200, 207]:
+        results = response.json()
+        for r in results:
+            print(f"Closed: {r.get('symbol')} - Qty: {r.get('qty')}")
+
--- main.py
+++ main.py
@@ -1,44 +1,69 @@
 import threading
 import json
-import time
-from alpaca_utils import get_current_price, place_order, close_position
-
 import math
+import time
+import datetime
+import pytz
 
-# NEEDS EOD "IF IN_POSITION CLOSE_POSITION AT X TIME" (needs datetime?)
-# NEEDS LOGIC TO STOP RUNNING AT X TIME (datetime?)
-    # SCHEDULED AUTOMATIC RUN... (04:00 - 22:00?)
-# NEEDS TRAILING STOP LOSS/TAKE PROFIT LOGIC (i think this needs yfinance data)
-# NEEDS TELEGRAM BOT NOTIFICATION INTEGRATION... (holy PITA)
+from alpaca_utils import get_current_price, place_order, close_position, close_all_positions
+
+# TRAILING STOP PLACEHOLDER (1.5%) IN CONFIG - REMEMBER TO CHANGE IT!
+
+# pushbullet notis
+# PIPE ENTRY/EXIT DATA TO CSV
+
+# SCHEDULING: START, STOP RUNNING AT X TIME
 # alpaca api keys, test logic/configs with paper trading
+
+eastern = pytz.timzone("US/Eastern")
+exit_open_positions_at = datetime.now(eastern).replace(hour=15, minute=55, second=0, microsecond=0)
+
+positions_closed = False
+
 
 def monitor_trade(config):
     symbol = config.symbol
     entry = config.entry_price
     stop = config.stop_loss
-    target = config.take_profit
+    trailing_stop = config.trailing_stop_percentage
     qty = math.ceil(config.dollar_value / config.entry_price)
+    day_high = entry
     in_position = False
+
+    print(f"[{symbol}] Monitoring...")
 
     while True:
         try:
+            now = datetime.now(eastern)
+            if now >= exit_open_positions_at:
+                global positions_closed
+                if not positions_closed:
+                    close_all_positions()
+                    positions_closed = True
+                    print("End of day - all positions closed.")
+                break
+
             price = get_current_price(symbol)
+
 
             if not in_position and price >= entry:
                 place_order(symbol, qty, "buy")
-                print(f"{qty} [{symbol}] bought at {price}")
+                print(f"{qty} [{symbol}] Market buy placed at {price}")
                 in_position = True
+                day_high = price
             
             elif in_position:
                 if price <= stop: 
                     close_position(symbol)
                     print(f"[{symbol}] stop-loss hit. Exiting.")
                     break
-                elif price >= target:
-                    # NEEDS TRAILING STOP-LOSS LOGIC
-                    close_position(symbol)
-                    print(f"[{symbol}] take-profit hit. Exiting.")
-                    break
+                else:
+                    if price > day_high:
+                        day_high = price
+                    if day_high >= entry * 1.15 and price <= day_high * (100 - trailing_stop)/100:
+                        close_position(symbol)
+                        print(f"[{symbol}] take-profit hit. Exiting.")
+                        break
 
             time.sleep(5)
 
@@ -51,5 +76,5 @@ with open("configs.json") as f:
     trade_setups = json.load(f)
 
 for setup in trade_setups:
-    t = threading.Thread(target=monitor_trade, args=(setup))
+    t = threading.Thread(target=monitor_trade, args=(setup,))
     t.start()
