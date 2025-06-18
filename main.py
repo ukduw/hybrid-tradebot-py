63e778f81636cbe5ab2dd5e2ccb1fe8b596ba816
ukduw <edhkmayo@gmail.com>
2025-06-18 05:18:53 -0400

    break > return ends thread more cleanly - remember to change back if something added after loop inside monitor_trade() function...

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def monitor_trade(setup):
                     close_all_positions()
                     positions_closed = True
                     print("End of day - all positions closed.")
-                break
+                return
 
             if not in_position and can_enter_trade() and price >= entry:
                 #place_order(symbol, qty)
@@ -82,7 +82,7 @@ def monitor_trade(setup):
                 pb.push_note("Hybrid bot", f"{qty} [{symbol}] Market buy placed at {price}")
             elif not in_position and price >= entry:
                 print(f"Skipped [{symbol}] @ {price}, PDT limit hit...")
-                break
+                return
 
             elif in_position:
                 if price <= stop: 
@@ -91,7 +91,7 @@ def monitor_trade(setup):
                     with open("trade-log/trade_log.txt", "a") as file:
                         file.write(f"{now},{symbol},Exit,{qty},{price}" + "\n")
                     pb.push_note(f"[{symbol}] stop-loss hit. Exiting.")
-                    break
+                    return
                 else:
                     if price > day_high:
                         day_high = price
@@ -101,7 +101,7 @@ def monitor_trade(setup):
                         with open("trade-log/trade_log.txt", "a") as file:
                             file.write(f"{now}, {symbol}, Exit, {qty}, {price}" + "\n")
                         pb.push_note(f"[{symbol}] take-profit hit. Exiting.")
-                        break
+                        return
 
             time.sleep(1)
 
