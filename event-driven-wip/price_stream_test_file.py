ca3fa721e3cfaa53272d0cbffd72aa46febd455e
ukduw <edhkmayo@gmail.com>
2025-07-31 09:29:10 -0400

    changed back to list; was throwing websocket errors related to async, fixed txt write statement to appropriate log, added threading to start statement for async issues

--- event-driven-wip/price_stream_test_file.py
+++ event-driven-wip/price_stream_test_file.py
@@ -5,15 +5,18 @@ from alpaca.data.enums import DataFeed
 from alpaca.trading.client import TradingClient
 
 import asyncio
-import time
 import datetime
 import pytz
+import time
+import threading
 
 from dotenv import load_dotenv
 import os
 
 eastern = pytz.timezone("US/Eastern")
 now = datetime.datetime.now(eastern)
+
+symbols = ["BKKT", ] # change for testing
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
@@ -26,11 +29,12 @@ stock_stream = StockDataStream(api_key=API_KEY, secret_key=SECRET_KEY, feed=Data
 
 async def handle_trade(trade: Trade):
     print(f"[WebSocket] {trade.symbol} @ {trade.price}") # comment out while not testing
-    with open("trade-log/trade_log.txt", "a") as file:
+    with open("event-driven-wip/price_stream_test_log.txt", "a") as file:
         file.write(f"{now},{trade.symbol},{trade.price}" + "\n")
 
-def start_price_stream(symbol):
-    stock_stream.subscribe_trades(handle_trade, symbol)
+def start_price_stream(symbols):
+    for symbol in symbols:
+        stock_stream.subscribe_trades(handle_trade, symbol)
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
@@ -48,6 +52,7 @@ def stop_price_stream(symbol):
 
 
 if __name__ == "__main__":
-    start_price_stream("BKKT") # change for testing
+    threading.Thread(target=start_price_stream, args=(symbols,), daemon=True).start()
     time.sleep(120)
-    stop_price_stream("BKKT")
+    for symbol in symbols:
+        stop_price_stream(symbol)
