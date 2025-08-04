32593bbb891f0f1a944350653af058857fb6f1b3
ukduw <edhkmayo@gmail.com>
2025-08-04 10:55:49 -0400

    fixed cleanup script for systemd - forgot to fix along with the sigterm/sigint cleanup; added imports, util import to unsubscribe per stock stream, removed asyncio, have to import the configs json to iterate through symbols to pass to util

--- cleanup.py
+++ cleanup.py
@@ -3,6 +3,10 @@ from alpaca.data.enums import DataFeed
 
 from dotenv import load_dotenv
 import os
+import json
+import sys
+
+from alpaca_utils import stop_price_stream
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
@@ -10,14 +14,30 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 stock_stream = StockDataStream(api_key=API_KEY, secret_key=SECRET_KEY, feed=DataFeed.SIP)
 
+
+CONFIG_PATH = "configs.json"
+with open("configs.json", "r") as f:
+    configs_json = json.load(f)
+
+symbols = [setup["symbol"] for setup in configs_json]
+
+
 async def main():
     try:
         print("Running cleanup...")
-        await stock_stream.unsubscribe_all()
+        for symbol in symbols:
+            await stop_price_stream(symbol)
         await stock_stream.stop_ws()
+
     except Exception as e:
         print(f"Cleanup error: {e}")
+    finally:
+        print("Cleanup complete. Exiting...")
+        sys.exit(0)
+    
 
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
+
+
