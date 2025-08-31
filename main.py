f5f32385c4a3411c5d8d58a8fe38ae287e1d7f8e
ukduw <edhkmayo@gmail.com>
2025-08-31 12:06:04 -0400

    re-wrote profit taking logic for rsi - updated imports for rsi util, rewrote profit taking logic for rsi, added another variable after realizing qty is set to cached/updated configure per loop, added extra if/else in high rsi case for 2nd take-profit, trail lowered to 15% (seems more appropriate for rsi, but test/tweak...)

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ while pb_reconnect_tries <= 5: # low due to risk of getting stuck in loop past p
         time.sleep(10)
 
 
-from alpaca_utils import start_price_quote_bar_stream, get_current_price, get_day_high, get_latest_macd, stop_price_quote_bar_stream, place_order, close_position, close_all_positions, stock_stream
+from alpaca_utils import start_price_quote_bar_stream, get_current_price, get_day_high, get_latest_macd, get_latest_rsi, stop_price_quote_bar_stream, place_order, close_position, close_all_positions, stock_stream
 
 eastern = pytz.timezone("US/Eastern")
 now = datetime.datetime.now(eastern)
@@ -91,6 +91,7 @@ symbols = [setup["symbol"] for setup in cached_configs]
     # write util to calculate rsi, re-write take-profit logic, test...
 # very good for short-term (maybe change to 10min candles to improve it?)
 # also not as good for longer runs - don't know how to account for these without making it worse at taking profit on short-term plays...
+    # maybe partial take-profit below certain threshold?
 
 
 async def monitor_trade(setup):
@@ -157,11 +158,11 @@ async def monitor_trade(setup):
                 await asyncio.sleep(1)
 
             if in_position:
-                macd = get_latest_macd(symbol)
-                percent_diff = ( macd['MACDh_12_26_9'] / macd['MACDs_12_26_9'] ) * 100 if macd['MACDs_12_26_9'] != 0 else 0
-                macd_perc_high = None
-
+                rsi = get_latest_rsi(symbol)
+                rsi_high = None
+                
                 half_position = round(qty / 2)
+                other_half = qty - half_position
 
                 if price < stop: 
                     close_position(symbol, qty)
@@ -171,18 +172,16 @@ async def monitor_trade(setup):
                     pb.push_note("Hybrid bot", f"[{symbol}] STOP-LOSS hit. Exiting @ {price}")
                     return
                 
-                if 150 < percent_diff < 200: # TWEAK VALUES
-                    macd_perc_high = percent_diff
-                    while not percent_diff > 199:
-                        macd = get_latest_macd(symbol)
-                        percent_diff = ( macd['MACDh_12_26_9'] / macd['MACDs_12_26_9'] ) * 100
+                if 75 <= rsi < 85: # TWEAK VALUES
+                    rsi_high = rsi
+                    while not rsi >= 85:
+                        rsi = get_latest_rsi(symbol)
                         
-                        if percent_diff > macd_perc_high:
-                            macd_perc_high = percent_diff
-                        if percent_diff <= macd_perc_high * 0.8: # TWEAK TRAIL
+                        if rsi > rsi_high:
+                            rsi_high = rsi
+                        if rsi <= rsi_high * 0.85: # TWEAK TRAIL
                             if not take_50:
                                 take_50 = True
-                                qty = qty - half_position
                                 close_position(symbol, half_position)
                                 print(f"[{symbol}] TAKE-PROFT hit. Exiting 50% position @ {price}")
                                 async with aiofiles.open("trade-log/trade_log.txt", "a") as file:
@@ -190,7 +189,7 @@ async def monitor_trade(setup):
                                 pb.push_note("Hybrid bot", f"[{symbol}] TAKE-PROFT hit. Exiting 50% position @ {price}")
                                 break
                             else:
-                                close_position(symbol, qty)
+                                close_position(symbol, other_half)
                                 print(f"[{symbol}] TAKE-PROFT hit. 2nd Exiting 50% position @ {price}")
                                 async with aiofiles.open("trade-log/trade_log.txt", "a") as file:
                                     await file.write(f"{now}, {symbol}, 2nd 50% Exit, {qty}, {price}" + "\n")
@@ -199,21 +198,28 @@ async def monitor_trade(setup):
                             
                         await asyncio.sleep(1)
 
-                if percent_diff > 199: # TWEAK VALUE
-                    macd_perc_high = percent_diff
+                if rsi >= 85: # TWEAK VALUE
+                    rsi_high = rsi
                     while True:
-                        macd = get_latest_macd(symbol)
-                        percent_diff = ( macd['MACDh_12_26_9'] / macd['MACDs_12_26_9'] ) * 100
+                        rsi = get_latest_rsi(symbol)
 
-                        if percent_diff > macd_perc_high:
-                            macd_perc_high = percent_diff
-                        if percent_diff <= macd_perc_high * 0.8: # TWEAK TRAIL
-                            close_position(symbol, qty)
-                            print(f"[{symbol}] TAKE-PROFT hit. Exiting 100% position @ {price}")
-                            async with aiofiles.open("trade-log/trade_log.txt", "a") as file:
-                                await file.write(f"{now}, {symbol}, 100% Exit, {half_position}, {price}" + "\n")
-                            pb.push_note("Hybrid bot", f"[{symbol}] TAKE-PROFT hit. Exiting 100% position @ {price}")
-                            return
+                        if rsi > rsi_high:
+                            rsi_high = rsi
+                        if rsi <= rsi_high * 0.85: # TWEAK TRAIL
+                            if not take_50:
+                                close_position(symbol, qty)
+                                print(f"[{symbol}] TAKE-PROFT hit. Exiting 100% position @ {price}")
+                                async with aiofiles.open("trade-log/trade_log.txt", "a") as file:
+                                    await file.write(f"{now}, {symbol}, 100% Exit, {half_position}, {price}" + "\n")
+                                pb.push_note("Hybrid bot", f"[{symbol}] TAKE-PROFT hit. Exiting 100% position @ {price}")
+                                return
+                            else:
+                                close_position(symbol, other_half)
+                                print(f"[{symbol}] TAKE-PROFT hit. 2nd Exiting 50% position @ {price}")
+                                async with aiofiles.open("trade-log/trade_log.txt", "a") as file:
+                                    await file.write(f"{now}, {symbol}, 2nd 50% Exit, {half_position}, {price}" + "\n")
+                                pb.push_note("Hybrid bot", f"[{symbol}] TAKE-PROFT hit. 2nd Exiting 50% position @ {price}")
+                                return
                         
                         await asyncio.sleep(1)
                 
